b76574288081d6cafd8996af0f13cb0a4873d08b
nso <nso@psud.nc>
2018-08-23 10:40:08 +1100

    ajout composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TotoComponent } from './toto/toto.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TotoComponent
   ],
   imports: [
     BrowserModule
